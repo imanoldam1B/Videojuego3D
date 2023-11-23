f83615edbbb5c482b3d4907866c2b3d256bfbc90
imanoldam1B <iperezadam1B@gmail.com>
2023-11-23 01:00:41 +0100

    Todos los videos completados a excepción del 9, buen funcionamiento pero falta escenario y demas

--- Videojuego3D_Imanol/Assets/Scripts/playerController.cs
+++ Videojuego3D_Imanol/Assets/Scripts/playerController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public CharacterController playerController;
+    public float gravityScale;
+    public Camera playerCamera;
 
     private Vector3 moveDirection;
+    private float yStore;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +20,26 @@ public class playerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        yStore = moveDirection.y;
+
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0f, Input.GetAxisRaw("Vertical"));
-        transform.position = transform.position + (moveDirection * moveSpeed * Time.deltaTime);
+        moveDirection = moveDirection * moveSpeed;
+        
+        moveDirection.y = yStore;   
+
+        //Salto
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        playerController.Move(moveDirection * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
+        
+
     }
 }
